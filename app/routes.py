ef6fb030fde7d5db73683a8453d19bd085ecd074
Timur <timmust98@gmail.com>
2024-03-10 18:30:41 +0300

    flask_2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        uname = form.username.data
+        pw = form.password.data
+        flash(f'Логин {uname}, {pw}', "info")
+        flash(f'Логин {uname}, {pw}', "danger")
+        flash(f'Логин {uname}, {pw}', "warning")
+        flash(f'Логин {uname}, {pw}', "success")
+        return redirect(url_for('login'))
+
+    return render_template('login.html', form=form, title="Вход")
